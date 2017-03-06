4450af82b74627dcfdf1e5d73a9a0e8919a5a105
Rob Miracle <rob@coronalabs.com>
2017-03-06 17:50:47 -0500

    Updated to modern Storyboard based launch images.

---
+++ LaunchImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchImage
+//
+//  Created by Rob Miracle on 2/22/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
